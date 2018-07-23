cb7d4f44ee04631a5168593ee2bf911f5a263932
Adward Stark <adwardstark@gmail.com>
2018-07-23 15:16:15 +0530

    Updated raw_data.js

--- api/routes/src/controllers/raw_data.js
+++ api/routes/src/controllers/raw_data.js
@@ -1,70 +1,78 @@
 'use strict';
+const DeviceStateConstants = require('../middlewares/stateconstants')
 var rp = require('request-promise');
 
-let getEMS_RawData = (req,res) => {
-    return new Promise(function(resolve,reject){
-
-    var accountID = req.params.accountID;
-    var siteID = req.params.siteID;
-    var relayID = req.query.relayID;
-    var deviceID = req.query.deviceID;
-    var beforeTime = req.query.beforeTime;
-    var afterTime = req.query.afterTime;
-
-    if(relayID !== undefined && deviceID !== undefined){
-
-        var deviceLogs = {
-            state:{
-                power: {
-                    value: [],
-                    timestamp: []
-                }
-                // ,
-                // brightness: {
-                //     value: [],
-                //     timestamp: []
-                // },
-                // hsl: {
-                //     value: [],
-                //     timestamp: []
-                // }
-            }
-        }
-
-        // Get device states
-        //req.dcs.getResourceState(siteID, `id=\"${deviceID}\"`, null).then(function(resp) {
-            
-            //var result = Object.keys(resp.state[deviceID]);
-
-            getDeviceLogs(req,accountID,siteID,relayID,deviceID).then(function(response){
-                response._embedded.logs.forEach((deviceEvent) => {
-                    if(deviceEvent.event.indexOf('power') > -1){
-                        deviceLogs.state.power.value.push(deviceEvent.metadata)
-                        deviceLogs.state.power.timestamp.push(deviceEvent.timestamp)
-                    }
-                })
-                //res.status(200).send(deviceLogs)
-               resolve(deviceLogs);
-            },function(err) {
-               reject(err)
-            });
-
-        // },function(err) {
-        //     reject(err)
-        // })
-    } else {
-        reject("Failure");
-    }
-});
+let getRawData = (req,res) => {
+    getEMS_RawData(req,"desc").then(function(response){
+       res.status(200).send(response);
+    }, function(error){
+       res.status(500).send(error)
+    });
 };
 
-let getDeviceLogs = (req,accountID,siteID,relayID,deviceID) => {
+let getEMS_RawData = (req, sortOrder) => {
+    
+    return new Promise(function(resolve,reject){
+    
+    // Param Values    
+    var accountID = req.params.accountID;
+    var siteID = req.params.siteID;
+    var relayID = req.params.relayID;
+    // Query Values
+    var deviceID = req.query.deviceID;
+    
+
+    var deviceLogs = { [deviceID]: { state:{ } } }
+
+    // Get device states
+    /*req.dcs.getResourceState(siteID, `id=\"${deviceID}\"`, null).then(function(resp) {*/
+            
+            //console.log(DeviceStateConstants.allstates.light_bulb_state)
+            var allDeviceStates = DeviceStateConstants.allstates.light_bulb_state
+            //var allDeviceStates = Object.keys(resp.state[deviceID]);
+            //console.log(allDeviceStates);
+
+            //var queryString = "&relay=" + relayID + "&device=" + deviceID + "&event=state-" + result[0];
+            var queryString = "&relay=" + relayID + "&device=" + deviceID + "&sort=" + sortOrder;
+            
+            getDeviceLogs(req,accountID,siteID,queryString).then(function(response){
+                //console.log(response);
+                allDeviceStates.forEach((stateID) => {
+                    var stateModel = {
+                                  value: [],
+                                  timestamp: []
+                                 }
+                    deviceLogs[deviceID].state[stateID] = stateModel
+                    //console.log(stateID)
+                    response._embedded.logs.forEach((deviceEvent) => {
+                        if(deviceEvent.event == "state-" + stateID){
+                            deviceLogs[deviceID].state[stateID].value.push(deviceEvent.metadata)
+                            deviceLogs[deviceID].state[stateID].timestamp.push(deviceEvent.timestamp)
+                            //console.log(deviceLogs)
+                        }
+                    })
+                })
+                resolve(deviceLogs); 
+            }, function(err){
+                reject(err)
+            });
+
+        /*},function(err) {
+            reject(err)
+        })*/
+    });
+};
+
+let getDeviceLogs = (req,accountID,siteID,queryString) => {
     return new Promise(function(resolve,reject){
     var cloudurl = req.headers.cloudurl;
     var authToken = req.headers.authorization;
 
-    var url = cloudurl + "/api/device_logs?account=" + accountID + "&site=" + siteID 
-              + "&relay=" + relayID + "&device=" + deviceID + "&sort=desc";
+    /*Old method 
+      var url = cloudurl + "/api/device_logs?account=" + accountID + "&site=" + siteID 
+              + "&relay=" + relayID + "&device=" + deviceID + "&event=state-" + stateID;*/
+
+      var url = cloudurl + "/api/device_logs?account=" + accountID + "&site=" + siteID + queryString;        
 
     var options = {
         uri: url,
@@ -88,6 +96,7 @@ let getDeviceLogs = (req,accountID,siteID,relayID,deviceID) => {
 };
 
 module.exports = {
+    getRawData,
     getEMS_RawData,
     getDeviceLogs
 }
